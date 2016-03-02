2420ee63ee0c79f2851a1f64b574e2243a362f8e
adrozdek <agata.drozdek@gmail.com>
2016-03-02 10:19:17 +0100

    Adding Ad, Category classes and FOSUserBundle. No relations yet

---
+++ README.md
@@ -0,0 +1,4 @@
+last_workshop
+=============
+
+A Symfony project created on March 2, 2016, 8:38 am.
---
+++ src/BoardBundle/Entity/Ad.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace BoardBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Ad
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="BoardBundle\Entity\AdRepository")
+ */
+class Ad
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=130)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="photoPath", type="string", length=255)
+     */
+    private $photoPath;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="expirationDate", type="datetime")
+     */
+    private $expirationDate;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Ad
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Ad
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set photoPath
+     *
+     * @param string $photoPath
+     * @return Ad
+     */
+    public function setPhotoPath($photoPath)
+    {
+        $this->photoPath = $photoPath;
+
+        return $this;
+    }
+
+    /**
+     * Get photoPath
+     *
+     * @return string 
+     */
+    public function getPhotoPath()
+    {
+        return $this->photoPath;
+    }
+
+    /**
+     * Set expirationDate
+     *
+     * @param \DateTime $expirationDate
+     * @return Ad
+     */
+    public function setExpirationDate($expirationDate)
+    {
+        $this->expirationDate = $expirationDate;
+
+        return $this;
+    }
+
+    /**
+     * Get expirationDate
+     *
+     * @return \DateTime 
+     */
+    public function getExpirationDate()
+    {
+        return $this->expirationDate;
+    }
+}
